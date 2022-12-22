d399c4387eee0c75a9b1c377cfb49a23cbc13549
marifedt-w <57739727+marifedt@users.noreply.github.com>
2022-12-21 20:54:45 -0500

    Add functions needed for the calculator
    add, subtract, multiply, divide, and operate

---
+++ index.js
@@ -0,0 +1,16 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
